7adffb0b416da1b004e4b3b755ee29c14783cf99
Santiago Lapeña <slapena88@gmail.com>
2023-06-15 10:03:30 +0200

    Coreccion en clase usuario

--- ClassLibrary1/Usuario.cs
+++ ClassLibrary1/Usuario.cs
@@ -11,18 +11,20 @@ namespace TP_Programacion3
     public class Usuario
     {
         public Usuario() { }
-        public Usuario(int id) {
-        this.ID = id;
+        public Usuario(int id)
+        {
+            this.ID = id;
         }
         public string nombre { get; set; }
         public string Apellido { get; set; }
         public int ID { get; set; }
-        public int DNI { get; set; }
+        public string DNI { get; set; }
         public MailAddress Mail { get; set; }
-        public int telefono { get; set; }
+        public string telefono { get; set; }
         public string direccion { get; set; }
         public bool estado { get; set; }
         public string URLimagen { get; set; }
+        public string contraseña { get; set; }
 
-    }   
+    }
 }
