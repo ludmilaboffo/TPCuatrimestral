2b1a00af0f9796990420a38d6f818e49f4c89318
FrancoPalacin <framaxlo07@gmail.com>
2023-07-04 01:01:49 -0300

    agregando baja y alta de usuarios por parte del administrador

--- TPCUATRI/BajaUsuario.aspx
+++ TPCUATRI/BajaUsuario.aspx
@@ -2,5 +2,17 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    <h1>Aca se hace la baja</h1>
+    <h1>LISTADO DE ARTISTAS</h1>
+    <asp:GridView ID="dgvArtistas" runat="server" CssClass="table" OnSelectedIndexChanged="dgvArtistas_SelectedIndexChanged" datakeynames="Id" AutoGenerateColumns="false" >
+        <Columns>  
+            <asp:BoundField HeaderText="ID" DataField="IdArtista" />
+            <asp:BoundField HeaderText="Nombre" DataField="Nombre" />
+            <asp:BoundField HeaderText="Apellido" DataField="Apellido" />
+            <asp:BoundField Headertext="Mail" DataField="Mail" />
+             <asp:BoundField Headertext="Telefono" DataField="Telefono" />
+             <asp:BoundField Headertext="Direccion" DataField="Direccion" />
+             <asp:BoundField Headertext="Estado" DataField="Estado" />
+            <asp:CommandField showselectbutton="true"  SelectText="Suspender" HeaderText="Accion" />
+        </Columns>
+    </asp:GridView>
 </asp:Content>
--- TPCUATRI/BajaUsuario.aspx.cs
+++ TPCUATRI/BajaUsuario.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Negocio;
+using Dominio;
 
 namespace TPCUATRI
 {
@@ -11,10 +13,31 @@ namespace TPCUATRI
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (!((Dominio.Usuario)Session["user"]).isAdmin())
+            if (!IsPostBack)
             {
-                Session.Add("error", "Solo los administradores acceden a esta sección");
-                Response.Redirect("Error.aspx", false);
+                if (!((Dominio.Usuario)Session["user"]).isAdmin())
+                {
+                    Session.Add("error", "Solo los administradores acceden a esta sección");
+                    Response.Redirect("Error.aspx", false);
+                }
+                ArtistasNegocio negocio = new ArtistasNegocio();
+                dgvArtistas.DataSource = negocio.ListarConSp();
+                dgvArtistas.DataBind();
+            }
+        }
+
+        protected void dgvArtistas_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ArtistasNegocio negocio = new ArtistasNegocio();
+            GridViewRow selectedRow = dgvArtistas.SelectedRow;
+            bool estado = Convert.ToBoolean(selectedRow.Cells[6].Text);
+            if (estado == true)
+            {
+                negocio.eliminarLogico((int)dgvArtistas.SelectedDataKey.Value, false);
+            }
+            else
+            {
+                negocio.eliminarLogico((int)dgvArtistas.SelectedDataKey.Value, true);
             }
         }
     }
